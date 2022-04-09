e53cb0f4abaa50f5640d15afb88327581ba17cee
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-04-09 15:15:44 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,56 +6,58 @@ template <typename T, int size>
 class TPQueue {
 
 private:
-	T arr[size];
-	int first;
-	int last;
+
+        T arr[size];
+        int first;
+        int last;
 
 public:
+
 	TPQueue() : first(0), last(0) {}
 
 	void push(T value) {
-		if (last - first >= size) {
-			throw std::string("Full");
-		} else {
-			int i = last-1;
-			bool flag = 0;
-			while ((i >= first) && (value.prior > arr[i].prior)) {
-				flag = 1;
-				arr[i + 1] = arr[i];
-				arr[i] = value;
+                if (last - first >= size) {
+                    throw std::string("Full");
+                  } else {
+                        int i = last-1;
+                        bool flag = 0;
+                        while ((i >= first) && (value.prior > arr[i].prior)) {
+                                flag = 1;
+                                arr[i + 1] = arr[i];
+                                arr[i] = value;
 				i--;
-			}
-			if (flag == 0) {
-				arr[last++] = value;
-				last++;
-			}
-		}
-	}
+                        }
+                        if (flag == 0) {
+                               arr[last++] = value;
+                               last++;
+                        }
+                }
+         }
 
-	T pop() {
-			return arr[(first++) % size];
-		}
+        T pop() {
+                    return arr[(first++) % size];
+                }
 
-	T front() {
-		if (first == last) {
-			throw std::string("Empty!");
-		} else {
-			return arr[first % size];
-		}
-	}
+        T front() {
+               if (first == last) {
+                   throw std::string("Empty!");
+                  } else {
+                    return arr[first % size];
+                }
+         }
 
-	T back() {
-		if (first == last) {
-			throw std::string("Empty!");
-		} else {
-			return arr[(last - 1) % size];
-		}
-	}
+        T back() {
+               if (first == last) {
+                     throw std::string("Empty!");
+                } else {
+                     return arr[(last - 1) % size];
+               }
+        }
 };
 
 struct SYM {
-	char ch;
-	int prior;
+       char ch;
+       int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
